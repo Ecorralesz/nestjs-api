fcc6c040fba2bf32ee5b6bc924c8212d7b84e1ed
Ecorralesz <estebcz@hotmail.com>
2023-05-31 00:15:57 -0500

    Updated the db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Welcome26!',
-      database: 'emails',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      host: 'aws.connect.psdb.cloud',
+      port: Number(3306),
+      username: 'xk9jom80dtw6v89pjkm0',
+      password: 'pscale_pw_lorN6ClmRPMVk8UnVejOTeQDZDg8aoSUsmkbFOAmtME',
+      database: 'kelloggs',
+      entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     EmailModule,
   ],
+   
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  config(); // Load environment variables from .env file
+
   const app = await NestFactory.create(AppModule);
-
   app.enableCors(); // Enable CORS for all routes
-
   await app.listen(3000);
-  "server running"
 }
 
 bootstrap();
